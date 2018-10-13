dbbb0cbeec681f66a44468b607a40e75956f4a31
EC2 Default User <ec2-user@ip-172-31-43-211.us-east-2.compute.internal>
2018-10-13 03:19:52 +0000

    cover 83.05%

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -6,25 +6,79 @@ require 'rails_helper'
 describe MoviesController do
 # the MoviesController class
     
-    describe 'instance method search (movies with the same director)' do
+    describe 'controller method search (movies with the same director)' do
     # the search method
         
         context 'When the specified movie has a director' do
+            
+            # do this before each 'it' block:
+            before :each do
+               # create an array of two “test double” Movie objects 
+               @fake_results = [double('movie1'), double('movie2')] 
+            end
+            
             it 'should call Movie.search_by_director(id) class method' do
-                expect(Movie).to receive(:search_by_director).with('asdf')
-                get :search, { :title => 'asdf' }
+                # set up a test double for Movie.search_by_director.
+                # search_by_director doesn't have to be defined in the movie model.
+                # Movie.search_by_director must be called to pass the test.
+                Movie.should_receive(:search_by_director).with('asdf').and_return(@fake_results)
+                # or: expect(Movie).to receive(:search_by_director).with('asdf').and_return(fake_results)
+                get :search, {:title => 'asdf'}
+            end
+            
+            it 'should render search view' do
+                # set up a test double for Movie.search_by_director.
+                # Movie.search_by_director doesn't have to be called to pass the test.
+                Movie.stub(:search_by_director).with('asdf').and_return(@fake_results)
+                get :search, {:title => 'asdf'}
+                response.should render_template('search')
             end
         end
         
         context 'When the specified movie has no director' do
-            it 'should go to the index page' do
+            it 'should render index view (home page)' do
                 Movie.stub(:search_by_director).with('asdf').and_return(nil)
-                get :search, { title: 'asdf' }
+                get :search, {:title => 'asdf'}
                 expect(response).to redirect_to(movies_path)
             end
         end
     
     end
     # method describe ends
+    
+    describe 'controller method index' do
+        
+        # create test object:
+        # Use let to define a memoized helper method. The value will be cached
+        # across multiple calls in the same example but not across examples.
+        
+        # Note that let is lazy-evaluated: it is not evaluated until the first time
+        # the method it defines is invoked. You can use let! to force the method's
+        # invocation before each example.
+        let!(:movie) {FactoryGirl.create(:movie)}
+        
+
+        it 'should render the index template' do
+            get :index
+            response.should render_template('index')
+        end
+
+        it 'should assign instance variable title_header with hilite' do
+            get :index, { sort: 'title'}
+            assigns(:title_header).should eql('hilite')
+        end
+    
+        it 'should assign instance variable release_date with hilite' do
+            get :index, { sort: 'release_date'}
+            assigns(:date_header).should eql('hilite')
+        end
+        # The last two it block coresponse to these codes in the controller:
+        #   when 'title'
+        #       ordering,@title_header = {:title => :asc}, 'hilite'
+        #   when 'release_date'
+        #       ordering,@date_header = {:release_date => :asc}, 'hilite'
+        #   end
+    end
+    
 end
 # class describe ends
